6fa2557ac9385dcb102e8353c9fcfab14b9fc073
Johnson Arapalo <user@sample.com>
2018-02-15 16:45:32 +0000

    FUBAR

--- include/matrice_autonomy/mission_control.h
+++ include/matrice_autonomy/mission_control.h
@@ -7,6 +7,11 @@ static double Deg_To_Rad( double degree)
     return degree * (PI/180.0);
 };
 
+static double Rad_To_Deg(double rad)
+{
+  return rad * (180.0/PI);  
+};
+
 class MissionControl
 {
 public:
@@ -15,10 +20,16 @@ public:
     void Land();
     void GoHome();
     void MonitoredTakeoff();
-
-
+    void ObtainControl();
 
  private:
+    ros::NodeHandle nh;
+    ros::ServiceClient ctrl_authority_service;
+    ros::ServiceClient drone_task_service;
+    ros::ServiceClient query_version_service;
+
+    bool CheckM100();  // checks if drone is Matrice 100
+    
 
 };
 
--- src/mission_control.cpp
+++ src/mission_control.cpp
@@ -1,2 +1,77 @@
 #include "mission_control.h"
 
+MissionControl::MissionControl()
+{
+    // Start Basic Services
+    ctrl_authority_service = nh.serviceClient<dji_sdk::SDKControlAuthority> ("dji_sdk/sdk_control_authority");
+    drone_task_service         = nh.serviceClient<dji_sdk::DroneTaskControl>("dji_sdk/drone_task_control");
+    query_version_service      = nh.serviceClient<dji_sdk::QueryDroneVersion>("dji_sdk/query_drone_version");
+
+}
+
+bool MissionControl::CheckM100()
+{
+    dji_sdk::QueryDroneVersion query;
+    query_version_service.call(query);
+
+    if(query.response.version == DJISDK::DroneFirmwareVersion::M100_31)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+
+
+}
+
+void MissionControl::Takeoff()
+{
+    dji_sdk::DroneTaskControl droneTaskControl;
+
+    droneTaskControl.request.task = dji_sdk::DroneTaskControl::Request::TASK_TAKEOFF;
+
+    drone_task_service.call(droneTaskControl);
+    ROS_INFO("DRONE TAKING OFF!");
+
+    if(!droneTaskControl.response.result)
+    {
+        ROS_ERROR("Drone failed to take off");
+    }
+
+
+}
+
+void MissionControl::Land()
+{
+   dji_sdk::DroneTaskControl droneTaskControl;
+
+    droneTaskControl.request.task = dji_sdk::DroneTaskControl::Request::TASK_LAND;
+
+    drone_task_service.call(droneTaskControl);
+
+    if(!droneTaskControl.response.result)
+    {
+        ROS_ERROR("Drone failed to land");
+    }
+
+}
+
+void MissionControl::ObtainControl()
+{
+    dji_sdk::SDKControlAuthority authority;
+    authority.request.control_enable = 1;
+    ctrl_authority_service.call(authority);
+    ROS_INFO("Program has obtained control");
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "teleop_matrice");
+
+    ros::spin();
+    return 0;
+
+}
